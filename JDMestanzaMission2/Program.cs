e839dc0ce659998b288978bdec852485fb1b3ae0
JDMestanzy <jd.mestanza.rod@gmail.com>
2023-01-18 17:00:17 -0700

    Add project files.

---
+++ JDMestanzaMission2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JDMestanzaMission2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
